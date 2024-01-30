af4e707b44dc4f39fe8e5af5c2201ea9fc00ffc1
MEDPC <mohamedlem216@gmail.com>
2024-01-31 00:01:22 +0100

    TLP-5 added JWT authentication middleware and fixed a typo in taskID ':'

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,18 +1,10 @@
 import express from 'express'
-
-import {
-	createTask,
-	deleteAllTasks,
-	deleteTask,
-	readAllTasks,
-	readTask,
-	updateTask,
-} from '../controllers/taskController.js'
+import { createTask, deleteTask, readAllTasks, readTask, updateTask } from '../controllers/taskController.js'
 import { authenticateWithJWT } from '../middlewares/authenticateWithJWT.js'
 
 const router = express.Router()
 
-router.get('/tasks', readAllTasks)
+router.get('/tasks', authenticateWithJWT, readAllTasks)
 router.get('/tasks/:taskID', readTask)
 router.post('/tasks', authenticateWithJWT, createTask)
 router.patch('/tasks/:taskID', authenticateWithJWT, updateTask)
