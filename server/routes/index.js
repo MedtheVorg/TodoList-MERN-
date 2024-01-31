94c06732559a2a327153fd8c68d31b95f3c99ecc
Mehdi ROCHDI <mehdirochdi@MacBook-Pro.local>
2024-01-31 12:18:49 +0100

    TLP-16 added comment route to global router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { taskRouter } from './taskRoute.js'
 import { userRouter } from './userRoutes.js'
+import { commentRoute } from './comment.routes.js'
 
 const router = express.Router()
 
+router.use('/api/comments', commentRoute)
 router.use('/api', taskRouter)
 router.use('/api/users', userRouter)
 
