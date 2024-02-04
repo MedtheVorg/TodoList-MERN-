61b58f3dbf4408b1abddaad49d40dd2bdaa0497a
MEDPC <mohamedlem216@gmail.com>
2024-02-04 17:08:26 +0100

    TLP-19 updated useStore hook

--- src/hooks/useStore.jsx
+++ src/hooks/useStore.jsx
@@ -1,42 +1,42 @@
-import { createContext, useContext, useReducer } from 'react'
+import { createContext, useContext, useState } from 'react'
 
 // store context where our react app state will be stored
 const StoreContext = createContext({})
 
-// initial state of useReducer
-const initialState = {}
-
-// reducer logic
-const reducer = (state, action) => {
-	switch (action.type) {
-		case 'LOGIN USER':
-			return { ...state, user: action.payload }
-
-		case 'LOGOUT USER':
-			return { ...state, user: null }
-		default:
-			return state
-	}
-}
-
 // custom hook to get the context value
-export const useStoreContext = () => {
+export const useStore = () => {
 	return useContext(StoreContext)
 }
 
-export const useStore = () => {
-	// initialize a useReducer with the store initialState
-	// useReducer returns an array [state, dispatch]
-	// state is our currentState (its the initialState Object)
-	// dispatch is a function that takes an ACTION as a string and
-	// mutate the state based on the defined logic we defined before in
-	// the reducer variable
-	return useReducer(reducer, initialState)
-}
-
-// eslint-disable-next-line react/prop-types
 export const StoreProvider = ({ children }) => {
-	const store = useStore()
+	const [user, setUser] = useState(null)
+	const [tasks, setTasks] = useState([])
+	const [updatedTask, setUpdatedTask] = useState({
+		title: '',
+		description: '',
+		priority: '',
+		status: '',
+		deadLine: '',
+	})
+	const [searchTerm, setSearchTerm] = useState('')
+	const [orderBy, setOrderBy] = useState('deadline')
 
-	return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+	return (
+		<StoreContext.Provider
+			value={{
+				user,
+				setUser,
+				tasks,
+				setTasks,
+				updatedTask,
+				setUpdatedTask,
+				searchTerm,
+				setSearchTerm,
+				orderBy,
+				setOrderBy,
+			}}
+		>
+			{children}
+		</StoreContext.Provider>
+	)
 }
